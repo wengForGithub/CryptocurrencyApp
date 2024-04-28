72e8f492984dcabe3117c0bec2dc84776d565a9d
tzuhsiang <36352393+wengForGithub@users.noreply.github.com>
2024-04-28 16:32:09 +0800

    sorted folder

--- NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class CoinsViewModel: ObservableObject {
+    
+    @Published var coin = ""
+    @Published var price = ""
+    
+    init() {
+        fetchPrice()
+    }
+    
+    func fetchPrice(){
+        let urlSting = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+        guard let url = URL(string: urlSting) else { return }
+        
+        print("Fetching price..")
+        
+//        this is asynchronous
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            print("Did receive data \(data)")
+        }.resume()
+        
+        print("Did reach end of function")
+        
+    }
+}
