f32812fe5d4ae88fff52e605f78f92bee1bd62fc
tzuhsiang <36352393+wengForGithub@users.noreply.github.com>
2024-04-28 14:36:44 +0800

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Owen on 2024/4/28.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
