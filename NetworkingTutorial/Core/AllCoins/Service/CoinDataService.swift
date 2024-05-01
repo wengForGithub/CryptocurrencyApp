f0adb7d9ec7a574a4c6f60d67e81069775864e36
tzuhsiang <36352393+wengForGithub@users.noreply.github.com>
2024-05-01 22:33:00 +0800

    Service Refactor With Generic HTTP Data Downloader

--- NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
+++ NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
@@ -7,58 +7,51 @@
 
 import Foundation
 
-class CoinDataService {
+protocol HTTPDataDownloader {
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T
+}
+
+extension HTTPDataDownloader {
+    // this is a reusable function and a Generic function
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T {
+        
+        guard let url = URL(string: endpoint) else {
+            throw CoinAPIError.requestFailed(desription: "Invalid URL")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw CoinAPIError.requestFailed(desription: "Request failed")
+        }
+        
+        guard httpResponse.statusCode == 200 else {
+            throw CoinAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
+        }
+        
+        // this is Generics, to decode any type of the data
+        do {
+            return try JSONDecoder().decode(type, from: data)
+        } catch {
+            print("DEBUG: Error \(error)")
+            throw error as? CoinAPIError ?? .unknownError(error: error)
+        }
+    }
+}
+
+class CoinDataService: HTTPDataDownloader {
     
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24&locale=en"
     
-    
-    
     func fetchCoins() async throws -> [Coin] {
-        guard let url = URL(string: urlString) else { return [] }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw CoinAPIError.requestFailed(desription: "Request failed")
-        }
-        
-        guard httpResponse.statusCode == 200 else {
-            throw CoinAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
-        }
-        
-        do {
-            let coins = try JSONDecoder().decode([Coin].self, from: data)
-            return coins
-        } catch {
-            print("DEBUG: Error \(error)")
-            throw error as? CoinAPIError ?? .unknownError(error: error)
-        }
+        return try await fetchData(as: [Coin].self, endpoint: urlString)
     }
     
     func fetchCoinDetails(id: String) async throws -> CoinDetails? {
-        
         let detailsUrlString = "https://api.coingecko.com/api/v3/coins/\(id)?localization=false"
-        
-        guard let url = URL(string: detailsUrlString) else { return nil }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw CoinAPIError.requestFailed(desription: "Request failed")
-        }
-        
-        guard httpResponse.statusCode == 200 else {
-            throw CoinAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
-        }
-        
-        do {
-            let details = try JSONDecoder().decode(CoinDetails.self, from: data)
-            return details
-        } catch {
-            print("DEBUG: Error \(error)")
-            throw error as? CoinAPIError ?? .unknownError(error: error)
-        }
+        return try await fetchData(as: CoinDetails.self, endpoint: detailsUrlString)
     }
+    
     
 }
 
--- NetworkingTutorial/Core/CoinDetails/ViewModel/CoinDetailsViewModel.swift
+++ NetworkingTutorial/Core/CoinDetails/ViewModel/CoinDetailsViewModel.swift
@@ -23,14 +23,13 @@ class CoinDetailsViewModel: ObservableObject {
     
     @MainActor
     func fetchCoinDetails() async {
-        print("DEBUG: Fetching coins")
-        try? await Task.sleep(nanoseconds: 2_000_000_000)
-        print("DEBUG: Task woke up")
+//        print("DEBUG: Fetching coins")
+//        try? await Task.sleep(nanoseconds: 2_000_000_000)
+//        print("DEBUG: Task woke up")
         
         do {
-            let details = try await service.fetchCoinDetails(id: coinId)
-            print("DEBUG: Details \(details)")
-            self.coinDetails = details
+            self.coinDetails = try await service.fetchCoinDetails(id: coinId)
+//            print("DEBUG: Details \(details)")
         } catch {
             print("DEBUG: Error \(error.localizedDescription)")
         }
